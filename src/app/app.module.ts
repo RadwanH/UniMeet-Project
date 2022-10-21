2c8f9d162f6a8cbbd67b817b57724876aa861a69
Emre Can Cakir <emrecan.cakir@piworks.net>
2022-10-21 20:58:11 +0300

    feat(login-page.module): add login-page module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { LoginPageModule } from './modules/login-page/login-page.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/login-page/login-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LoginPageModule { }
