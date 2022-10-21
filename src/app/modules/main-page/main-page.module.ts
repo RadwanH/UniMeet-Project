1358aca3ac982d0fabcd0b3284cc8936f8304603
Emre Can Cakir <emrecan.cakir@piworks.net>
2022-10-21 21:35:39 +0300

    feat(main-page): add main-page module & component

---
+++ src/app/modules/main-page/main-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainPageComponent } from './main-page/main-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MainPageModule { }
