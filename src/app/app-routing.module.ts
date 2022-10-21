698a86aca3b0d5500c4f6dfb8bfb813123ec2de8
Emre Can Cakir <emrecan.cakir@piworks.net>
2022-10-21 21:36:26 +0300

    feat(app routing): add component's routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
+import { MainPageComponent } from './modules/main-page/main-page/main-page.component';
+import { LoginPageComponent } from './modules/login-page/login-page/login-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'mainPage', component: MainPageComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
