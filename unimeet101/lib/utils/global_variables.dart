4a11fd903ca86971a14b0c4695f9849f095752dd
RadwanH <rrh@eskisehir.edu.tr>
2022-12-01 21:08:54 +0300

    feat(mobile app): add search for user page
    add a page to search for users in real time
    
    #29

--- unimeet101/lib/utils/global_variables.dart
+++ unimeet101/lib/utils/global_variables.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:unimeet101/screens/add_post_screen.dart';
 import 'package:unimeet101/screens/feed_screen.dart';
+import 'package:unimeet101/screens/search_screen.dart';
 
 const webScreenSize = 600;
 const homeScreenItems = [
   FeedScreen(),
-  Center(
-    child: Text('chat'),
-  ),
+  SearchScreen(),
   AddPostScreen(),
   Center(
     child: Text('notification'),
